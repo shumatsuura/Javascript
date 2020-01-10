701c1af14c820d52645703a46428e45d5284a093
SHUNSUKE MATSUURA <shunsuke@SHUNSUKEnoMacBook-Air.local>
2020-01-10 16:18:28 +0900

    課題提出

--- JavaScriptシリーズ課題/scoring.js
+++ JavaScriptシリーズ課題/scoring.js
@@ -18,20 +18,50 @@ $(document).ready(function(){
     $("#sum_indicate").text(sum);
 
     // ここに、上記を参考にして平均点を出力する処理を書き込む
+    let average = sum / subject_points.length;
+    $('#average_indicate').text(average);
   };
 
   function get_achievement(){
     // ここに、ランクの値の文字列（平均点が80点以上なら"A"、60点以上なら"B"、40点以上なら"C"、それ以下なら"D"）を出力する処理を書き込む
+    let average = Number($('#average_indicate').text());
+    switch(true){
+      case(average >= 80):
+      return "A";
+
+      case(average >= 60):
+      return "B";
+
+      case(average >= 40):
+      return "C";
+
+      default:
+      return "D";
+    }
   }
 
   function get_pass_or_failure(){
     // ここに、全ての教科が60点以上なら"合格"の文字列、一つでも60点未満の教科があったら"不合格"の文字列を出す処理を書き込む
+    let judge = "合格";
+    let subject_points = [Number($('#national_language').val()),
+                          Number($('#english').val()),
+                          Number($('#mathematics').val()),
+                          Number($('#science').val()),
+                          Number($('#society').val())
+                          ];
+    for(let i = 0 ; i < subject_points.length ; i++){
+      if(subject_points[i] < 60){
+        judge = "不合格";
+        break;
+      }
+    }
+    return judge;
   }
 
   function judgement(){
     // ここに、「最終ジャッジ」のボタンを押したら「あなたの成績はAです。合格です」といった内容を出力する処理を書き込む
     // 下記の記述をすることで、「最終ジャッジ」のボタンを押すと「あなたの成績は（ここに「ランク」の値を入れる）です。（ここに「判定」の値を入れる）です」という文字の入った水色のフキダシが出力される処理が実装される。
-    $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">あなたの成績は（ここに「ランク」の値を入れる）です。（ここに「判定」の値を入れる）です</label>`);
+    $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">あなたの成績は${$('#evaluation').text()}です。${$('#judge').text()}です</label>`);
   };
 
   $('#national_language, #english, #mathematics, #science, #society').change(function() {
@@ -39,11 +69,15 @@ $(document).ready(function(){
   });
 
   $('#btn-evaluation').click(function() {
-    get_achievement();
+    //get_achievement();
+    let evaluation = get_achievement();
+    $('#evaluation').text(evaluation);
   });
 
   $('#btn-judge').click(function() {
-    get_pass_or_failure();
+    //get_pass_or_failure();
+    let judge = get_pass_or_failure();
+    $('#judge').text(judge);
   });
 
   $('#btn-declaration').click(function() {
